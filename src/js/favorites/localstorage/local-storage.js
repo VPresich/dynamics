cde49ca5a45ebf13edb87a6bff6af37596852651
VPresich <valya.alexandrova.presich@gmail.com>
2024-01-24 11:10:27 +0100

    Add class for LocalStorage

---
+++ src/js/favorites/localstorage/local-storage.js
@@ -0,0 +1,44 @@
+import Library from './library';
+
+class LocalStorageLib {
+  #localStorageKey = 'favorites';
+  dataLibrary = [];
+
+  constructor(key) {
+    this.#localStorageKey = key || this.#localStorageKey;
+    this.dataLibrary = new Library([]);
+  }
+
+  saveData() {
+    localStorage.setItem(
+      this.#localStorageKey,
+      JSON.stringify(this.dataLibrary.data)
+    );
+    alert('Data saved to Local Storage!');
+  }
+
+  getData() {
+    const strSavedData = localStorage.getItem(this.#localStorageKey);
+    return (this.dataLibrary.data = strSavedData
+      ? JSON.parse(strSavedData)
+      : []);
+  }
+
+  addObj(obj) {
+    this.getData();
+    if (!this.isObjSaved(obj.id)) {
+      this.dataLibrary.addObj(obj);
+      this.saveData();
+      console.log('Saved');
+    } else {
+      console.log('Not saved');
+    }
+  }
+
+  isObjSaved(id) {
+    this.getData();
+    return this.dataLibrary.getObjById(id) !== undefined;
+  }
+}
+
+export default LocalStorageLib;
