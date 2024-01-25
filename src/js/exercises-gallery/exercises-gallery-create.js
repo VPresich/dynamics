be7d56c8582c0d7144fde5bbac1628762f882db0
kovalsofiia1 <kovalsofiia1@gmail.com>
2024-01-25 23:56:17 +0200

    fixed markup and render

--- src/js/exercises-gallery/exercises-gallery-create.js
+++ src/js/exercises-gallery/exercises-gallery-create.js
@@ -1,7 +1,9 @@
 import exercisesGalleryMarkup from './exercises-gallery-markup';
 import galleryCreate from '../common/gallery-create';
 
-const exercisesGalleryCreate = (exercisesContainer, exercises) =>
+const exercisesGalleryCreate = (filtersContainer, exercises) => {
+  filtersContainer.innerHTML = '<ul class="exercise-gallery"></ul>'
+  const exercisesContainer = document.querySelector('.exercise-gallery')
   galleryCreate(exercises, exercisesContainer, exercisesGalleryMarkup);
-
+}
 export default exercisesGalleryCreate;
