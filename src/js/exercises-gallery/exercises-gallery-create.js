fec5296686c94b1394fa83aa5889012a91a28bfe
kovalsofiia1 <kovalsofiia1@gmail.com>
2024-01-26 12:18:29 +0200

    changed exercisesGalleryCreate

--- src/js/exercises-gallery/exercises-gallery-create.js
+++ src/js/exercises-gallery/exercises-gallery-create.js
@@ -1,7 +1,9 @@
 import exercisesGalleryMarkup from './exercises-gallery-markup';
 import galleryCreate from '../common/gallery-create';
 
-const exercisesGalleryCreate = (exercisesContainer, exercises) =>
+const exercisesGalleryCreate = (filtersContainer, exercises) => {
+  filtersContainer.innerHTML = '<ul class="exercises-gallery"></ul>'
+  const exercisesContainer = document.querySelector('.exercises-gallery')
   galleryCreate(exercises, exercisesContainer, exercisesGalleryMarkup);
-
+}
 export default exercisesGalleryCreate;
