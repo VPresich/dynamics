39615658193d969cb8b3f0c21c03cf771147c469
Irinaaissa <irinaalisa7506@gmail.com>
2024-01-27 21:10:12 +0200

    h

---
+++ src/js/exercise-modal/exercise-modal-markup.js
@@ -0,0 +1,116 @@
+// const exercises = [
+//   {
+//     _id: '64f389465ae26083f39b17b3',
+//     bodyPart: 'back',
+//     equipment: 'barbell',
+//     gifUrl: 'https://ftp.goit.study/img/power-pulse/gifs/0022.gif',
+//     name: 'barbell pullover to press',
+//     target: 'lats',
+//     description: 'These large back muscles are responsible for shoulder',
+//     rating: 4.17,
+//     burnedCalories: 33,
+//     time: 3,
+//     popularity: 168,
+//   },
+// ];
+
+function modalWindowMarkup(filters = {}) {
+  const {
+    _id,
+    bodyPart,
+    equipment,
+    time,
+    target,
+    burnedCalories,
+    gifUrl,
+    name,
+    filter,
+    popularity,
+    rating,
+    description,
+  } = filters;
+
+  return `
+        <div class="exercise-window">
+          <div class="exercise-container">
+            <button class="exercise-close-btn" type="button">
+              <svg
+                class="exercise-close-icon"
+                width="28"
+                height="28"
+                aria-label="Close icon"
+              >
+                <use href="../img/icons/symbols.svg#icon-cross"></use>
+              </svg>
+            </button>
+            <div class="exercise-image-wrapper">
+              <img
+                src="${gifUrl}"
+                alt=""
+                class="exercise-image-modal"
+              />
+            </div>
+            <div class="exercise-info-wrapper">
+              <div class="exercise-name-and-rating-container">
+                <p class="exercise-name">${name}</p>
+                <p class="modal-rating">${rating}
+                  <svg class="exercise-star" width="18" height="18" aria-label="Star icon">
+                    <use href="../img/icons/symbols.svg#icon-star"></use>
+                 </svg>
+                </p>
+              </div>
+              <svg class="exercise-line-svg" aria-label="Line">
+               <use href="../img/icons/symbols.svg#line-svg"></use>
+              </svg>
+              <div class="exercise-params-container">
+                <ul class="exercise-params">
+                  <li class="exercise-params-card">
+                    <p class="exercise-param-name">Target</p>
+                    <p class="exercise-param-value exercise-param-target">${target}</p>
+                  </li>
+                  <li class="exercise-params-card">
+                    <p class="exercise-param-name">Body part</p>
+                    <p class="exercise-param-value exercise-param-bodypart">${bodyPart}</p>
+                  </li>
+                  <li class="exercise-params-card">
+                    <p class="exercise-param-name">Equipment</p>
+                    <p class="exercise-param-value exercise-param-equipment">${equipment}</p>
+                  </li>
+                  <li class="exercise-params-card">
+                    <p class="exercise-param-name">Popular</p>
+                    <p class="exercise-param-value exercise-param-popularuty">${popularity}</p>
+                  </li>
+                </ul>
+                <p class="exercise-calories">Burned calories</p>
+                <p class="exercise-calories-value">${burnedCalories}/${time} m</p>
+              </div>
+              <svg class="exercise-line-svg" aria-label="Line">
+               <use href="../img/icons/symbols.svg#line-svg"></use>
+              </svg>
+              <p class="exercise-description">${description}</p>
+              <div class="exercise-buttons">
+                <button
+                  class="exercise-favorite-btn"
+                  type="submit"
+                  data="exercise-id"
+                >
+                  Add to favorites
+                  <svg
+                  class="exercise-fav-icon"
+                  aria-label="Heart icon"
+                  >
+                  <use href="../img/icons/symbols.svg#icon-heart"></use>
+                  </svg>
+                </button>
+                <button class="exercise-raiting-btn" type="submit" data="${_id}">
+                  Give a rating
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>   
+
+      `;
+}
+
+export default modalWindowMarkup;
---
+++ src/js/favorites/favorites-exercises-section.js
@@ -0,0 +1,152 @@
+import axios from 'axios';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const favoriteslistExercises = document.querySelector(
+  '.favorites-list-exercises-likes'
+);
+const butRemovExercises = document.querySelector('.favorites-button-remove');
+
+const instance = axios.create({
+  baseURL: 'https://energyflow.b.goit.study/api',
+});
+
+const newArrLocalStorage = [];
+
+if (localStorage.length > 2) {
+  try {
+    newArrLocalStorage.push(JSON.parse(localStorage.getItem('')));
+
+    const newArr = newArrLocalStorage.flatMap(res => res);
+
+    const renderListFavoriteExercises = renderElement(newArr);
+    favoriteslistExercises.insertAdjacentHTML(
+      'beforeend',
+      renderListFavoriteExercises
+    );
+
+    newArr.map(res => console.log(res._id));
+
+    favoriteslistExercises.addEventListener('click', event => {
+      const deleteId = event.target.id;
+
+      if (deleteId) {
+        document
+          .querySelector(`.list-exercises-favorites[data-listId="${deleteId}"]`)
+          .remove();
+        newArr.map(res => {
+          if (res._id === deleteId) {
+            const csdvs = newArr.indexOf(res);
+
+            newArr.splice(csdvs, 1);
+
+            localStorage.setItem('arr', JSON.stringify(newArr));
+
+            if (!newArr.length) {
+              localStorage.removeItem('arr');
+
+              favoriteslistExercises.insertAdjacentHTML(
+                'beforeend',
+                `<div class="favortes-no-exercises-found">
+                <img
+                  class="favortes-no-exercises-found-img"
+                  src="./img/favorite/dumbbell-desktop@1x.jpg"
+                  alt="dumbbell-desktop"
+                />
+                <p class="favortes-no-exercises-found-text">
+                  It appears that you haven't added any exercises to your favorites
+                  yet. To get started, you can add exercises that you like to your
+                  favorites for easier access in the future.
+                </p>
+              </div>`
+              );
+            }
+          }
+        });
+      }
+    });
+  } catch (error) {
+    console.log('Parse form storage error');
+  }
+} else {
+  favoriteslistExercises.insertAdjacentHTML(
+    'beforeend',
+    `<div class="favortes-no-exercises-found">
+    <img
+      class="favortes-no-exercises-found-img"
+      src="./img/favorite/dumbbell-desktop@1x.jpg"
+      alt="dumbbell-desktop"
+    />
+    <p class="favortes-no-exercises-found-text">
+      It appears that you haven't added any exercises to your favorites
+      yet. To get started, you can add exercises that you like to your
+      favorites for easier access in the future.
+    </p>
+  </div>`
+  );
+}
+
+function renderElement(params) {
+  const render = params.reduce(
+    (html, { bodyPart, burnedCalories, name, target, _id }) =>
+      html +
+      ` <li data-listId="${_id}" class="list-exercises-favorites">
+            <div class="list-exercises-favorites-category">
+                <div>
+                    <p class="exercises-favorites-name-category">WORKOUT</p>
+                    <button id="${_id}" class="favorites-button-remove" type="button">
+                    <svg id="${_id}" class="icon icon-trash">
+                    <use id="${_id}"
+                        xlink:href="./img/icons/symbols.svg#icon-trash"
+                    ></use>
+                </svg>
+                    </button>
+                </div>
+
+                <button
+                    type="submit"
+                    class="exercises-favorites-but-start"
+                >
+                Start<svg class="icon icon-arrow">
+                <use
+                    xlink:href="./img/icons/symbols.svg#icon-arrow"
+                ></use>
+            </svg>
+                </button>
+            </div>
+            <div class="list-exercises-favorites-name">
+                <svg class="social-icon-run" width="24" height="24">
+                <use
+                href="./img/icons/symbols.svg#icon-run-exercises"
+            ></use>
+                </svg>
+                <p class="exercises-favorites-name">${name}</p>
+            </div>
+            <div class="list-exercises-favorites-conteiner">
+                <ul class="list-exercises-favorites-characteristic">
+                    <li class="favorites-list-characteristic">
+                        <span class="characteristic-criteria"
+                            >Burned calories: </span
+                        >${burnedCalories} / 3 min
+                    </li>
+                    <li class="favorites-list-characteristic">
+                        <span class="characteristic-criteria"
+                            >Body part: </span
+                        >${bodyPart}
+                    </li>
+                    <li class="favorites-list-characteristic">
+                        <span class="characteristic-criteria"
+                            >Target: </span
+                        >${target}
+                    </li>
+                </ul>
+            </div>
+        </li>`,
+    ''
+  );
+  return render;
+}
+
+// const renderListFavoriteExercises = renderElement(newArr);
+
+// listExercises.insertAdjacentHTML('beforeend', renderListFavoriteExercises);
