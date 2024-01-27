17474afba6427e7293076150085fa080fb0a9200
VPresich <111353667+VPresich@users.noreply.github.com>
2024-01-27 18:22:31 +0000

    Deploying to gh-pages from @ VPresich/dynamics@0cc00f0b115880f2deb63c996c1545bf77978e15 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import"./assets/styles-971650b8.js";import{a as e}from"./assets/vendor-0a7943b3.js";const o=document.querySelector(".favorites-quote-day-title"),a=e.create({baseURL:"https://energyflow.b.goit.study/api"});a.get("/quote ").then(t=>{o.insertAdjacentHTML("beforeend",` <p class="favorites-quote-day-text">
+import"./assets/styles-20da2f95.js";import{a as e}from"./assets/vendor-0a7943b3.js";const o=document.querySelector(".favorites-quote-day-title"),a=e.create({baseURL:"https://energyflow.b.goit.study/api"});a.get("/quote ").then(t=>{o.insertAdjacentHTML("beforeend",` <p class="favorites-quote-day-text">
                 ${t.data.quote}
                 </p>
                     <p class="favorites-quote-day-text-autor">${t.data.author}</p>`)}).catch(t=>console.log(t));
