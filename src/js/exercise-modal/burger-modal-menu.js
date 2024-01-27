b4a3545d79fe4c9e4ed30f45523d75be54dc57e2
Irinaaissa <irinaalisa7506@gmail.com>
2024-01-27 18:33:43 +0200

    18,33,,,27,01

--- src/js/exercise-modal/burger-modal-menu.js
+++ src/js/exercise-modal/burger-modal-menu.js
@@ -1,6 +1,8 @@
+
+
 const buttonMenuRef = document.querySelector('.js-open-menu-btn');
- const mobileMenuRef = document.querySelector('.js-mobile-menu');
- const buttonCloseMenuRef = document.querySelector('.js-mobile-menu-close-btn');
+const mobileMenuRef = document.querySelector('.js-mobile-menu');
+const buttonCloseMenuRef = document.querySelector('.js-mobile-menu-close-btn');
 
 buttonMenuRef.addEventListener('click', () => {
 mobileMenuRef.classList.add('is-open');
---
+++ src/js/exercise-modal/social-list.js
@@ -0,0 +1,30 @@
+const iconFacebookUrl = "https://www.facebook.com/goITclub/";
+const iconInstagramUrl = "https://www.instagram.com/goitclub/";
+const iconYoutubeUrl = "https://www.youtube.com/c/GoIT";
+
+function createIconMarkup(url, label) {
+  return `
+    <li class="social-list-item">
+      <a class="social-list-link" target="_blank" rel="noopener" href="${url}">
+        <svg class="social-list-icon" width="28" height="28" aria-label="${label} icon">
+          <use href="../img/icons/symbols.svg#icon-${label.toLowerCase()}"></use>
+        </svg>
+      </a>
+    </li>
+  `;
+}
+
+const socialList = document.querySelector('.social-list-menu');
+const ulElement = document.querySelector('.social-list-header');
+
+const iconsMarkup = `
+  ${createIconMarkup(iconFacebookUrl, "Facebook")}
+  ${createIconMarkup(iconInstagramUrl, "Instagram")}
+  ${createIconMarkup(iconYoutubeUrl, "Youtube")}
+`;
+
+ulElement.insertAdjacentHTML('beforeend', iconsMarkup);
+socialList.insertAdjacentHTML('beforeend', iconsMarkup);
+
+
+export default sendRequest;
