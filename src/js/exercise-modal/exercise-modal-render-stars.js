8ef6d8bbc375c81097980fda29c67204a87a059b
genbaku <140975610+genbaku@users.noreply.github.com>
2024-01-27 20:33:49 +0100

    remake-func

---
+++ src/js/exercise-modal/exercise-modal-render-stars.js
@@ -0,0 +1,27 @@
+function renderRatingStars(ratingElement) {
+    const rating = parseInt(ratingElement.textContent);
+    ratingElement.innerHTML = '';
+  
+    for (let i = 0; i < 5; i++) {
+      const starIcon = document.createElement('svg');
+      starIcon.classList.add('exercise-star');
+      starIcon.setAttribute('width', '18');
+      starIcon.setAttribute('height', '18');
+      starIcon.setAttribute('aria-label', 'Star icon');
+  
+      if (i < rating) {
+        starIcon.innerHTML = '<use href="../img/icons/symbols.svg#icon-star"></use>';
+        starIcon.style.fill = 'yellow';
+      } else {
+        starIcon.innerHTML = '<use href="../img/icons/symbols.svg#icon-star"></use>';
+        starIcon.style.fill = 'grey';
+      }
+  
+      ratingElement.appendChild(starIcon);
+    }
+  }
+  window.onload = function() {
+    const ratingElement = document.querySelector('.modal-rating');
+    renderRatingStars(ratingElement);
+  };
+  
