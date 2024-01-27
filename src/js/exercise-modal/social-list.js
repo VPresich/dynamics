07edf1de0fbecb5c806edbc2cae58c03967777a8
Irinaaissa <irinaalisa7506@gmail.com>
2024-01-28 00:02:10 +0200

    1

--- src/js/exercise-modal/social-list.js
+++ src/js/exercise-modal/social-list.js
@@ -7,15 +7,24 @@ function createIconMarkup(url, label) {
     <li class="social-list-item">
       <a class="social-list-link" target="_blank" rel="noopener" href="${url}">
         <svg class="social-list-icon" width="28" height="28" aria-label="${label} icon">
-          <use href="../img/icons/symbols.svg#icon-${label.toLowerCase()}"></use>
+          <use href="../img/icons/symbols.svg#${label.toLowerCase()}"></use>
         </svg>
       </a>
     </li>
   `;
 }
+const element = document.querySelector('#your-element-id');
+
+if (element) {
+  element.insertAdjacentHTML('position', 'text');
+} else {
+  console.error('Елемент не знайдено');
+}
 
 const socialList = document.querySelector('.social-list-menu');
+// const socialListFooter = document.querySelector('.social-list');
 const ulElement = document.querySelector('.social-list-header');
+
 
 const iconsMarkup = `
   ${createIconMarkup(iconFacebookUrl, "Facebook")}
@@ -25,3 +34,6 @@ const iconsMarkup = `
 
 ulElement.insertAdjacentHTML('beforeend', iconsMarkup);
 socialList.insertAdjacentHTML('beforeend', iconsMarkup);
+// socialListFooter.insertAdjacentHTML('beforeend', iconsMarkup);
+
+
