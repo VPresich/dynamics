fd5ce87156cac50281b4c94ebe80d0217a825641
Irinaaissa <irinaalisa7506@gmail.com>
2024-01-28 12:08:26 +0200

    вернула иконки без джави , не знаю чому не працює додайте поки так будь ласка,

--- src/js/exercise-modal/social-list.js
+++ src/js/exercise-modal/social-list.js
@@ -7,7 +7,7 @@ function createIconMarkup(url, label) {
     <li class="social-list-item">
       <a class="social-list-link" target="_blank" rel="noopener" href="${url}">
         <svg class="social-list-icon" width="28" height="28" aria-label="${label} icon">
-          <use href="../img/icons/symbols.svg#${label.toLowerCase()}"></use>
+          <use href="../img/icons/symbols.svg#${label.replace(/\s/g, "")}"></use>
         </svg>
       </a>
     </li>
@@ -15,11 +15,6 @@ function createIconMarkup(url, label) {
 }
 const element = document.querySelector('#your-element-id');
 
-if (element) {
-  element.insertAdjacentHTML('position', 'text');
-} else {
-  console.error('Елемент не знайдено');
-}
 
 const socialList = document.querySelector('.social-list-menu');
 // const socialListFooter = document.querySelector('.social-list');
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-// import './js/common/social-icons.js';
-// import './js/header/header.js';
+//  import './js/exercise-modal/social-list.js'
+  import './js/exercise-modal/burger-modal-menu.js';
 import './js/quote/quote-handles.js';
 import './js/exercise-filters/exercise-filters-handles.js';
 import './js/exercise-modal/exercise-modal-handles.js';
