675987e95907c9820181bc1da94413844245a086
genbaku <140975610+genbaku@users.noreply.github.com>
2024-01-29 17:43:47 +0100

    add-close-icon

--- src/js/exercise-modal/exercise-modal-markup.js
+++ src/js/exercise-modal/exercise-modal-markup.js
@@ -42,7 +42,7 @@ function modalWindowMarkup(filters = {}) {
                 height="28"
                 aria-label="Close icon"
               >
-                <use href="${iconURL}#icon-close"></use>
+                <use href="${iconURL}#icon-close-modal"></use>
               </svg>
             </button>
             <div class="exercise-image-wrapper">
