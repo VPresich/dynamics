7bf87e96501f59e40f6a8a8266dc10c4eeeb2742
VPresich <valya.alexandrova.presich@gmail.com>
2024-01-31 09:08:36 +0100

    email refactoring

---
+++ src/js/footer/messages.js
@@ -0,0 +1,9 @@
+export const SUCCESS_MSG =
+  "We're excited to have you on board! 🎉 Thank you for subscribing to new exercises on Energy Flow. You've just taken a significant step towards improving your fitness and well-being.";
+
+export const ERROR409_MSG = 'Subscription already exists';
+export const WRONG_EMAIL = 'Please, enter a valid email address';
+export const ERROR_FETCH = 'Sorry! Something is wrong';
+export const NO_EMAIL = 'Please enter email';
+
+export const EMAIL_PATERN = /^[w-]+(.[w-]+)*@([w-]+.)+[a-zA-Z]{2,7}$/;
